4061b0a9186de3fb5498590d448443daf25339c7
Martin Thrömer <63214013+meedox-de@users.noreply.github.com>
2025-02-02 19:39:05 +0100

    adding minecraft-clone

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { Game } from './game/Game.js';
-
-// Warte auf DOM-Load
-document.addEventListener('DOMContentLoaded', function() {
-    const game = new Game();
-    game.init().catch(error => {
-        console.error('Fehler beim Initialisieren des Spiels:', error);
-    });
-});
