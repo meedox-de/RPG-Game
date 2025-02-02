5e97af4931a2b31b90c2253d1cb91ab2b6945185
Martin Thrömer <63214013+meedox-de@users.noreply.github.com>
2025-02-02 19:44:53 +0100

    fix minecraft-clone problems

--- scripts/world.js
+++ scripts/world.js
@@ -2,6 +2,19 @@ import * as THREE from 'three';
 import { WorldChunk } from './worldChunk';
 import { DataStore } from './dataStore';
 
+// Polyfill für requestIdleCallback
+window.requestIdleCallback = window.requestIdleCallback || function(cb) {
+    const start = Date.now();
+    return setTimeout(function() {
+        cb({
+            didTimeout: false,
+            timeRemaining: function() {
+                return Math.max(0, 50 - (Date.now() - start));
+            }
+        });
+    }, 1);
+};
+
 export class World extends THREE.Group {
 
   /**
